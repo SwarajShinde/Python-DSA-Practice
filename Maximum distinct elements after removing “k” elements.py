5d9852e1c9f710a0e36cc83d95f4ca71412eaaa1
SwarajShinde <41779790+SwarajShinde@users.noreply.github.com>
2020-05-22 11:37:00 +0530

    Create Maximum distinct elements after removing “k” elements.py

---
+++ Maximum distinct elements after removing “k” elements.py
@@ -0,0 +1,31 @@
+from collections import Counter
+from heapq import heappop, heappush, heapify 
+t=int(input())
+for _ in range(t):
+    n,m = map(int,input().split())
+    arr = list(map(int,input().split()))
+    counts = Counter(arr)
+    s=dict()
+    for i in arr:
+        s[i] = counts[i]
+
+    lis = []
+    for i in s:
+        lis.append(s[i])
+    
+    heap = []
+    heapify(heap)
+   
+    for i in lis:
+        heappush(heap,-1*i)
+    
+    while m!=0:
+        a=heappop(heap)
+        a=a+1
+        if a!=0:
+            heappush(heap,a)
+        
+        m = m-1
+    print(len(heap))
+
+    
